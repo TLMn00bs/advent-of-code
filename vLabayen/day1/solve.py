6a741454f1ad82c4201d80809353aae9a27c74e4
vLabayen <victor.mendi3216@gmail.com>
2020-12-10 21:02:58 +0100

    expand combinations variables

--- vLabayen/day1/solve.py
+++ vLabayen/day1/solve.py
@@ -6,19 +6,19 @@ from itertools import combinations
 # Step by step
 with open('input.txt') as f:
 	nums = [int(line[:-1]) for line in f]
-	for c in combinations(nums, 2):
-		if c[0] + c[1] == 2020: print(c[0] * c[1])
+	for x,y in combinations(nums, 2):
+		if x + y == 2020: print(x * y)
 
 # As one-liner
-with open('input.txt') as f: print([c[0]*c[1] for c in combinations((int(line[:-1]) for line in f), 2) if (c[0] + c[1] == 2020)][0])
+with open('input.txt') as f: print([x*y for x,y in combinations((int(line[:-1]) for line in f), 2) if (x + y == 2020)][0])
 
 
 # Puzzle 2
 # Step by step
 with open('input.txt') as f:
 	nums = [int(line[:-1]) for line in f]
-	for c in combinations(nums, 3):
-		if c[0] + c[1] + c[2] == 2020: print(c[0] * c[1] * c[2])
+	for x,y,z in combinations(nums, 3):
+		if x + y + z == 2020: print(x * y * z)
 
 # As one-liner
-with open('input.txt') as f: print([c[0]*c[1]*c[2] for c in combinations((int(line[:-1]) for line in f), 3) if (c[0] + c[1] + c[2] == 2020)][0])
+with open('input.txt') as f: print([x * y * z for x,y,z in combinations((int(line[:-1]) for line in f), 3) if (x + y + z == 2020)][0])
