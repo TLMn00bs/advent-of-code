2be4b7cbec83c5657eaaec4d615fd8cb4a3ee6f0
vLabayen <victor.mendi3216@gmail.com>
2021-12-03 18:04:45 +0100

    Refactor 2021-d1

--- vLabayen/2021/d1/d1_rs/src/main.rs
+++ vLabayen/2021/d1/d1_rs/src/main.rs
@@ -5,17 +5,11 @@ use std::io::{BufRead, BufReader};
 
 fn p1(filename: &String) {
 	let mut n = 0;
-	let mut current_depth: i32;
-
-	let file = File::open(filename).unwrap();
-	let reader = BufReader::new(file);
-	let mut data_gen = reader.lines();
-
-	let next = data_gen.next();
-	match next {
-		Some(depth) => current_depth = depth.unwrap().parse::<i32>().unwrap(),
-		None => process::exit(1)
-	}
+	let mut data_gen = BufReader::new(File::open(filename).unwrap()).lines();
+	let mut current_depth = match data_gen.next() {
+		Some(depth) => depth.unwrap().parse::<i32>().unwrap(),
+		None => panic!("File is empty")
+	};
 	
 	for (_, line) in data_gen.enumerate() {
 		let depth = line.unwrap().parse::<i32>().unwrap();
@@ -31,36 +25,29 @@ fn p2(filename: &String) {
 	let mut n = 0;
 	let mut sliding_window:[i32; 3] = [0, 0, 0];
 	let mut last_idx = 0;
-
-	let file = File::open(filename).unwrap();
-	let reader = BufReader::new(file);
-	let mut data_gen = reader.lines();
+	let mut data_gen = BufReader::new(File::open(filename).unwrap()).lines();
 
 	// Read the first 3 items of the file to prefill the sliding window
 	for idx in 0..3 {
-		let item = data_gen.next();
-		match item {
-			Some(depth) => sliding_window[idx] = depth.unwrap().parse::<i32>().unwrap(),
-			None => process::exit(1)
-		}
+		sliding_window[idx] = match data_gen.next() {
+			Some(depth) => depth.unwrap().parse::<i32>().unwrap(),
+			None => panic!("File has only {} lines", idx)
+		};
 	}
 
 	// Read the remaining depths
 	for (_, line) in data_gen.enumerate() {
 		let past_sum:i32 = sliding_window.iter().sum();
-
-		let depth = line.unwrap().parse::<i32>().unwrap();
-                sliding_window[last_idx] = depth;
-
-		// To avoid shifting the whole window, that in this case is just 3, but could be larger,
-		// keep an index of the last depth to override it each time.
-		last_idx = (last_idx + 1) % 3;				// 3 is the length of the sliding_window
-                let current_sum:i32 = sliding_window.iter().sum();
+		sliding_window[last_idx] = line.unwrap().parse::<i32>().unwrap();
+		let current_sum:i32 = sliding_window.iter().sum();
 
                 if current_sum > past_sum {
                         n += 1;
                 }
 
+		// To avoid shifting the whole window, that in this case is just 3 but could be larger,
+		// keep an index of the last depth to override it each time.
+		last_idx = (last_idx + 1) % 3;				// 3 is the length of the sliding_window
         }
         println!("Number of depth increments: {}", n);
 }
