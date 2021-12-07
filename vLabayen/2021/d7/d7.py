3e1eb4f50ce9ff1ff1e5bbd42edb479251c8ea3e
vLabayen <victor.mendi3216@gmail.com>
2021-12-07 07:54:39 +0100

    2021 - d7.py: Refactor & small fix

--- vLabayen/2021/d7/d7.py
+++ vLabayen/2021/d7/d7.py
@@ -4,12 +4,7 @@ from numpy import median
 def p1(args):
 	with open(args.file, 'r') as f: hpositions = [int(h) for h in f.readline().strip().split(',')]
 	min_h, max_h = min(hpositions), max(hpositions)
-
-	best_fuel = sum(abs(hp - min_h) for hp in hpositions)
-	for h in range(min_h + 1, max_h + 1):
-		fuel = sum(abs(hp - h) for hp in hpositions)
-		if fuel < best_fuel: best_fuel = fuel
-
+	best_fuel = min(sum(abs(hp - h) for hp in hpositions) for h in range(min_h, max_h + 1))
 	print(best_fuel)
 
 def fuel_usage(start, end):
@@ -19,7 +14,7 @@ def fuel_usage(start, end):
 def p2(args):
 	with open(args.file, 'r') as f: hpositions = [int(h) for h in f.readline().strip().split(',')]
 	min_h, max_h = min(hpositions), max(hpositions)
-	best_fuel = min(sum(fuel_usage(h, hp) for hp in hpositions) for h in range(min_h, max_h))
+	best_fuel = min(sum(fuel_usage(h, hp) for hp in hpositions) for h in range(min_h, max_h + 1))
 	print(best_fuel)
 
 if __name__ == '__main__':
