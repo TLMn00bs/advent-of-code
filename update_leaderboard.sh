267bea2bf15fabd8c29d1826b297b3de2e55502e
jartigag <jartigag@pm.me>
2021-12-09 15:43:53 +0100

    fixes: s/0rb3/i97orbegozo/, merge leaderboard into master

--- update_leaderboard.sh
+++ update_leaderboard.sh
@@ -16,7 +16,7 @@ EOF
 
 curl --cookie "session=$COOKIE" -XGET https://adventofcode.com/2021/leaderboard/private/view/1065002.json 2>/dev/null > leaderboard2021.json
 
-data=$(jq '.members | flatten | sort_by(.stars) | reverse | .[] | {name: .name, stars: .stars}' leaderboard2021.json | sed 's/Jesús Arellano/jesusarell/')
+data=$(jq '.members | flatten | sort_by(.stars) | reverse | .[] | {name: .name, stars: .stars}' leaderboard2021.json | sed 's/Jesús Arellano/jesusarell/' | sed 's/0rb3/i97orbegozo/')
 
 echo $data | jq -j '.name," ",.stars,"\n"' | \
 awk 'BEGIN{
@@ -26,7 +26,7 @@ awk 'BEGIN{
         langs["jartigag"]   ="Typescript"
         langs["mariaove"]   ="Java"
         langs["jesusarell"] ="Python"
-        langs["0rb3"]       ="Python"
+        langs["i97orbegozo"]="Python"
     }
     {print" | [@"$1"](https://github.com/"$1")| ["langs[$1]"](https://github.com/TLMn00bs/advent-of-code/tree/master/"$1") | "$2" |"}' >> README.md
 
