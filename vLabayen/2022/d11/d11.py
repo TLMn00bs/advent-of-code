feaafed9ab1e8e1458992af6f07b2dd0e8a993dc
vLabayen <victor.mendi3216@gmail.com>
2022-12-18 00:11:35 +0100

    d11 p2

--- vLabayen/2022/d11/d11.py
+++ vLabayen/2022/d11/d11.py
@@ -18,7 +18,19 @@ def p1(args):
 	print(reduce(lambda acc, num: acc * num, top_inspected_items, 1))
 
 def p2(args):
-	pass
+	monkeys = read_file(args.file)
+
+	# To keep numbers in a asumible size, use a relief action that applies a circular arithmetic
+	# using the mcm the divisible_by property of every monkey.
+	# Since all seems to be primes, and there is no real requirement to use the real mcm,
+	# just multiply all together to get a common multiple of all divisors.
+	# Nevertheless, we can easyly remove duplicate factors.
+	divisors = set(monkey.divisible_by for monkey in monkeys)
+	mcm = reduce(lambda acc, divisor: acc * divisor, divisors, 1)
+
+	for _ in repeat(None, 10_000): run_round(monkeys, lambda worry_level: worry_level % mcm)
+	top_inspected_items = sorted([m.num_inspected_items for m in monkeys], reverse=True)[:2]
+	print(reduce(lambda acc, num: acc * num, top_inspected_items, 1))
 
 if __name__ == '__main__':
 	import argparse
