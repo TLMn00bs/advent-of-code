4ad0662d5fb457f4d10dce4e327580c0ea2b4468
jartigag <jartigag@pm.me>
2023-12-02 21:04:05 +0100

    fix

--- update_leaderboard.sh
+++ update_leaderboard.sh
@@ -16,19 +16,19 @@ EOF
 
 curl --cookie "session=$COOKIE" -XGET https://adventofcode.com/2023/leaderboard/private/view/1065002.json 2>/dev/null > leaderboard2023.json
 
-data=$(jq '.members | flatten | sort_by(.stars) | reverse | .[] | {name: .name, stars: .stars}' leaderboard2023.json | sed 's/Rubén Maté/rubenmate/' | sed 's/Jesús Arellano/jesusarell/' | sed 's/0rb3/i97orbegozo/')
+data=$(jq '.members | flatten | sort_by(.stars) | reverse | .[] | {name: .name, stars: .stars}' leaderboard2023.json | sed 's/Rubén Maté/rubenmate/' | sed 's/calvo111979/anatxiki/' | sed 's/Jesús Arellano/jesusarell/' | sed 's/0rb3/i97orbegozo/')
 
 echo $data | jq -j '.name," ",.stars,"\n"' | \
 awk 'BEGIN{
         langs["lassa97"]    ="*"
-        langs["vLabayen"]   ="*"
-        langs["calvo111979"]="*"
+        langs["vLabayen"]   ="Python"
+        langs["anatxiki"]   ="Python"
         langs["jartigag"]   ="*"
         langs["mariaove"]   ="*"
         langs["jesusarell"] ="*"
         langs["i97orbegozo"]="*"
         langs["rubenmate"]  ="*"
-        langs["adrian-uri"] ="*"
+        langs["Adrian-uri"] ="*"
         langs["clarriu97"]  ="*"
     }
     {print" | [@"$1"](https://github.com/"$1")| ["langs[$1]"](https://github.com/TLMn00bs/advent-of-code/tree/master/"$1") | "$2" |"}' >> README.md
@@ -47,13 +47,13 @@ EOF
 
 curl --cookie "session=$COOKIE" -XGET https://adventofcode.com/2022/leaderboard/private/view/1065002.json 2>/dev/null > leaderboard2022.json
 
-data=$(jq '.members | flatten | sort_by(.stars) | reverse | .[] | {name: .name, stars: .stars}' leaderboard2022.json | sed 's/Rubén Maté/rubenmate/'  | sed 's/Jesús Arellano/jesusarell/' | sed 's/0rb3/i97orbegozo/')
+data=$(jq '.members | flatten | sort_by(.stars) | reverse | .[] | {name: .name, stars: .stars}' leaderboard2022.json | sed 's/Rubén Maté/rubenmate/' | sed 's/calvo111979/anatxiki/' | sed 's/Jesús Arellano/jesusarell/' | sed 's/0rb3/i97orbegozo/')
 
 echo $data | jq -j '.name," ",.stars,"\n"' | \
 awk 'BEGIN{
         langs["lassa97"]    ="*"
         langs["vLabayen"]   ="Python"
-        langs["calvo111979"]="*"
+        langs["anatxiki"]   ="*"
         langs["jartigag"]   ="*"
         langs["mariaove"]   ="*"
         langs["jesusarell"] ="*"
@@ -75,13 +75,13 @@ EOF
 
 curl --cookie "session=$COOKIE" -XGET https://adventofcode.com/2021/leaderboard/private/view/1065002.json 2>/dev/null > leaderboard2021.json
 
-data=$(jq '.members | flatten | sort_by(.stars) | reverse | .[] | {name: .name, stars: .stars}' leaderboard2021.json | sed 's/Rubén Maté/rubenmate/'  | sed 's/Jesús Arellano/jesusarell/' | sed 's/0rb3/i97orbegozo/')
+data=$(jq '.members | flatten | sort_by(.stars) | reverse | .[] | {name: .name, stars: .stars}' leaderboard2021.json | sed 's/Rubén Maté/rubenmate/' | sed 's/calvo111979/anatxiki/' | sed 's/Jesús Arellano/jesusarell/' | sed 's/0rb3/i97orbegozo/')
 
 echo $data | jq -j '.name," ",.stars,"\n"' | \
 awk 'BEGIN{
         langs["lassa97"]    ="Python (after day 3, using grinch.py to grab solutions from other people)"
         langs["vLabayen"]   ="Python (Rust a few days later)"
-        langs["calvo111979"]="Python (Rust a few days later)"
+        langs["anatxiki"]   ="Python (Rust a few days later)"
         langs["jartigag"]   ="Typescript"
         langs["mariaove"]   ="Java"
         langs["jesusarell"] ="Python"
@@ -104,7 +104,7 @@ EOF
 
 curl --cookie "session=$COOKIE" -XGET https://adventofcode.com/2020/leaderboard/private/view/1065002.json 2>/dev/null > leaderboard2020.json
 
-data=$(jq '.members | flatten | sort_by(.stars) | reverse | .[] | {name: .name, stars: .stars}' leaderboard2020.json | sed 's/Rubén Maté/rubenmate/'  | sed 's/Jesús Arellano/jesusarell/' | sed 's/0rb3/i97orbegozo/')
+data=$(jq '.members | flatten | sort_by(.stars) | reverse | .[] | {name: .name, stars: .stars}' leaderboard2020.json | sed 's/Rubén Maté/rubenmate/' | sed 's/calvo111979/anatxiki/' | sed 's/Jesús Arellano/jesusarell/' | sed 's/0rb3/i97orbegozo/')
 
 # fix order:
 data=$(echo $data | sed 's/{ "name": "lassa97", "stars": 50 } { "name": "vLabayen", "stars": 50 }/{ "name": "vLabayen", "stars": 50 } { "name": "lassa97", "stars": 50 }/')
@@ -113,7 +113,7 @@ echo $data | jq -j '.name," ",.stars,"\n"' | \
 awk 'BEGIN{
         langs["lassa97"]    ="Python"
         langs["vLabayen"]   ="Python"
-        langs["calvo111979"]="Python"
+        langs["anatxiki"]   ="Python"
         langs["jartigag"]   ="Python"
         langs["mariaove"]   ="Java"
         langs["jesusarell"] ="Python"
