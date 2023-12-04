b2ae5f61b7955947f42d5ba7f45b5218473dca8e
anatxiki <acalvoinfa10@gmail.comls>
2023-12-04 18:33:16 +0100

    refactor: split

--- anatxiki/2023/day4/day4.py
+++ anatxiki/2023/day4/day4.py
@@ -16,7 +16,6 @@ def update_array_of_games(my_array, number_of_winning_games, index):
     starting_point = index
     ending_point = index + number_of_winning_games
     number_of_copies = my_array[index]
-    print(number_of_copies)
     for i in range(starting_point, ending_point):
         my_array[i+1] += number_of_copies
     return
@@ -30,14 +29,13 @@ with open('input.txt', 'r') as file:
 
     for idx, line in enumerate(lines):
         data, mynumbers = line.split('|')
-        winning_numbers = [x for x in data.split(':')[-1].split(' ') if x != '']
-        mynumbers = [x for x in mynumbers.strip().split(' ') if x != '']
+        winning_numbers = [x for x in data.split(':')[-1].split()]
+        mynumbers = [x for x in mynumbers.strip().split()]
 
         number_of_winning_numbers = get_number_of_winning_numbers(winning_numbers,mynumbers)
         update_array_of_games(array_of_games, number_of_winning_numbers, idx)
         
         solution_part1 += get_points(number_of_winning_numbers)
-        print(number_of_winning_numbers, array_of_games)
         
     print('solution part 1',solution_part1)
     print('solution part 2', sum(array_of_games))
