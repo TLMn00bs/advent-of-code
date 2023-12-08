90a12961f0daa11307e61d95ab753835e7e76574
vlabayen <victor.labayen@naudit.es>
2023-12-08 10:24:41 +0100

    part 1

---
+++ vLabayen/2023/d8/d8.py
@@ -0,0 +1,58 @@
+import logging
+from typing import List, Tuple
+from attrs import define
+from enum import Enum
+import re
+
+class Step(Enum):
+	LEFT  = 'L'
+	RIGHT = 'R'
+
+def steps_gen(steps: List[Step]):
+	while True:
+		for step in steps: yield step
+
+@define
+class Node:
+	name : str
+	left : str
+	right: str
+
+parse_node_rgx = re.compile(r'([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)')
+def read_file(file: str) -> Tuple[List[Step], List[Node]]:
+	with open(file, 'r') as f:
+		lines = (l.strip() for l in f)
+
+		steps = [Step(step) for step in next(lines)]
+		next(lines)
+		nodes = [Node(*parse_node_rgx.match(node).groups()) for node in lines]	# type: ignore
+
+	return steps, nodes
+
+def p1(args):
+	steps, nodes = read_file(args.file)
+	steps_network = {node.name: node for node in nodes}
+
+	current_node = steps_network['AAA']
+	for i, step in enumerate(steps_gen(steps)):
+		next_node_name = current_node.right if step == Step.RIGHT else current_node.left
+		current_node = steps_network[next_node_name]
+
+		if current_node.name == 'ZZZ': break
+	
+	print(i + 1)	# type: ignore
+
+def p2(args):
+	_ = read_file(args.file)
+
+if __name__ == '__main__':
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('file', type=str, default='input.txt')
+	parser.add_argument('-v', '--verbose', type=str, choices={'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'}, default='WARNING')
+	args = parser.parse_args()
+
+	logging.basicConfig(level=args.verbose)
+
+	p1(args)
+	p2(args)
