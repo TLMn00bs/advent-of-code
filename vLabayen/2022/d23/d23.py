7686a798dfd270aaed2c57facc264109063ae92d
vlabayen <victor.labayen@naudit.es>
2023-12-09 02:33:22 +0100

    refactor

--- vLabayen/2022/d23/d23.py
+++ vLabayen/2022/d23/d23.py
@@ -1,44 +1,12 @@
 #!/bin/python3
 import logging
 from attrs import define, field
-from typing import Tuple, Iterable, Set, List, Callable, Dict
-from enum import Enum
+from typing import Tuple, Iterable, Set, List, Dict, Optional
 from collections import defaultdict
 from itertools import count
 
 Coordinate = Tuple[int, int]
 
-class Direction(Enum):
-	N  = ( 0, -1)
-	S  = ( 0, +1)
-	W  = (-1,  0)
-	E  = (+1,  0)
-	NE = (+1, -1)
-	NW = (-1, -1)
-	SE = (+1, +1)
-	SW = (-1, +1)
-
-def move(position: Coordinate, direction: Direction) -> Coordinate:
-	x, y = position
-	dx, dy = direction.value
-	return (x + dx, y + dy)
-
-def adyacent_positions(position: Coordinate) -> Iterable[Coordinate]:
-	for direction in Direction: yield move(position, direction)
-
-def is_position_occupied(position: Coordinate, direction: Direction, elves: Set[Coordinate]) -> bool:
-	return move(position, direction) in elves
-
-def should_propose_move(position: Coordinate, elves: Set[Coordinate]) -> bool:
-	return any(is_position_occupied(position, d, elves) for d in Direction)
-
-def propose_move_in_direction(position: Coordinate, elves: Set[Coordinate], directions: List[Direction]) -> bool:
-	return not any(is_position_occupied(position, d, elves) for d in directions)
-
-def should_propose_move_north(position, elves): return propose_move_in_direction(position, elves, [Direction.N, Direction.NE, Direction.NW])
-def should_propose_move_south(position, elves): return propose_move_in_direction(position, elves, [Direction.S, Direction.SE, Direction.SW])
-def should_propose_move_west (position, elves): return propose_move_in_direction(position, elves, [Direction.W, Direction.NW, Direction.SW])
-def should_propose_move_east (position, elves): return propose_move_in_direction(position, elves, [Direction.E, Direction.NE, Direction.SE])
 
 def print_elves(elves: Set[Coordinate], title: str) -> None:
 	max_x, min_x = max(x for x, y in elves), min(x for x, y in elves)
@@ -64,41 +32,6 @@ def read_file(file: str) -> Iterable[Coordinate]:
 			for x, c in enumerate(line):
 				if c == '#': yield x, y
 
-def p1(args):
-	elves = set(read_file(args.file))
-	proposing_order: List[Tuple[Callable[[Coordinate, Set[Coordinate]], bool], Direction]] = [
-		(should_propose_move_north, Direction.N),
-		(should_propose_move_south, Direction.S),
-		(should_propose_move_west , Direction.W),
-		(should_propose_move_east , Direction.E),
-	]
-
-	# print_elves(elves, 'Initial State')
-
-	for round in range(10):
-		round_proposing_order = [proposing_order[(round + i) % 4] for i in range(4)]
-
-		elves_proposing_to_move = sorted([elve for elve in elves if should_propose_move(elve, elves)], key = lambda elve: elve[::-1])
-
-		elves_proposed_destinations: Dict[Coordinate, Coordinate] = {}
-		requested_destinations = defaultdict(lambda: 0)
-		for elve in elves_proposing_to_move:
-			for propose_to_move, direction in round_proposing_order:
-				if propose_to_move(elve, elves):
-					destination = move(elve, direction)
-					elves_proposed_destinations[elve] = destination
-					requested_destinations[destination] += 1
-					break
-
-		new_destinations = set(destination for destination, num_times_requested in requested_destinations.items() if num_times_requested == 1)
-		moving_elves = set(elve for elve, destionation in elves_proposed_destinations.items() if destionation in new_destinations)
-
-		elves.difference_update(moving_elves)
-		elves.update(new_destinations)
-		# print_elves(elves, f'End of Round {round + 1}')
-
-	print(get_containing_rectangle_area(elves) - len(elves))
-
 @define
 class SoilMap:
 	elves: Set[Coordinate]
@@ -110,42 +43,14 @@ class SoilMap:
 	should_move_east : Dict[Coordinate, bool] = field(init=False, factory=dict)
 
 	def __attrs_post_init__(self):
-		for (x, y) in self.elves:
-			self.has_neighbours[(x, y)] = any((
-				(x - 1, y - 1) in self.elves,
-				(x    , y - 1) in self.elves,
-				(x + 1, y - 1) in self.elves,
-				(x - 1, y    ) in self.elves,
-				(x + 1, y    ) in self.elves,
-				(x - 1, y + 1) in self.elves,
-				(x    , y + 1) in self.elves,
-				(x + 1, y + 1) in self.elves,
-			))
-			self.should_move_north[(x, y)] = not any((
-				(x - 1, y - 1) in self.elves,
-				(x    , y - 1) in self.elves,
-				(x + 1, y - 1) in self.elves,
-			))
-			self.should_move_south[(x, y)] = not any((
-				(x - 1, y + 1) in self.elves,
-				(x    , y + 1) in self.elves,
-				(x + 1, y + 1) in self.elves,
-			))
-			self.should_move_west[(x, y)] = not any((
-				(x - 1, y - 1) in self.elves,
-				(x - 1, y    ) in self.elves,
-				(x - 1, y + 1) in self.elves,
-			))
-			self.should_move_east[(x, y)] = not any((
-				(x + 1, y - 1) in self.elves,
-				(x + 1, y    ) in self.elves,
-				(x + 1, y + 1) in self.elves,
-			))
+		self.inner_update()
 
 	def update(self, remove: Set[Coordinate], add: Set[Coordinate]):
 		self.elves.difference_update(remove)
 		self.elves.update(add)
+		self.inner_update()
 
+	def inner_update(self):
 		for (x, y) in self.elves:
 			self.has_neighbours[(x, y)] = any((
 				(x - 1, y - 1) in self.elves,
@@ -178,8 +83,8 @@ class SoilMap:
 				(x + 1, y + 1) in self.elves,
 			))
 
-def p2(args):
-	soil_map = SoilMap(set(read_file(args.file)))
+
+def run(soil_map: SoilMap, max_rounds: Optional[int] = None) -> int:
 	proposing_order: List[Tuple[Dict[Coordinate, bool], int, int]] = [
 		(soil_map.should_move_north,  0, -1),
 		(soil_map.should_move_south,  0, +1),
@@ -187,11 +92,13 @@ def p2(args):
 		(soil_map.should_move_east , +1,  0),
 	]
 
+	def rounds():
+		if max_rounds is None: return count()
+		assert max_rounds > 0
+		return range(max_rounds)
+
 	# print_elves(soil_map.elves, 'Initial State')
-
-	for round in count():
-		if round % 10 == 0: print(round)
-
+	for round in rounds():
 		round_proposing_order = [proposing_order[(round + i) % 4] for i in range(4)]
 		elves_proposed_destinations: Dict[Coordinate, Coordinate] = {}
 		requested_destinations = defaultdict(lambda: 0)
@@ -210,12 +117,21 @@ def p2(args):
 		new_destinations = set(destination for destination, num_times_requested in requested_destinations.items() if num_times_requested == 1)
 		moving_elves = set(elve for elve, destination in elves_proposed_destinations.items() if destination in new_destinations)
 
-		if len(new_destinations) == 0:
-			print(round + 1)
-			return
-
+		if len(new_destinations) == 0: return round + 1
 		soil_map.update(moving_elves, new_destinations)
 		# print_elves(soil_map.elves, f'End of Round {round + 1}')
+
+	return round + 1	# type: ignore
+
+def p1(args):
+	soil_map = SoilMap(set(read_file(args.file)))
+	_ = run(soil_map, max_rounds=10)
+	print(get_containing_rectangle_area(soil_map.elves) - len(soil_map.elves))
+
+def p2(args):
+	soil_map = SoilMap(set(read_file(args.file)))
+	num_rounds = run(soil_map)
+	print(num_rounds)
 
 
 if __name__ == '__main__':
