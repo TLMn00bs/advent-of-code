817e82217cc84b77dce3d7d6396420d65d5ac7e3
vlabayen <victor.labayen@naudit.es>
2023-12-09 12:29:25 +0100

    part 1

---
+++ vLabayen/2023/d9/d9.py
@@ -0,0 +1,45 @@
+import logging
+from typing import Iterable, List
+
+
+def read_file(file: str) -> Iterable[List[int]]:
+	with open(file, 'r') as f:
+		for line in (l.strip() for l in f):
+			yield [int(n) for n in line.split()]
+
+
+# py3.10: from itertools import pairwise
+def pairwise(serie: List[int]) -> Iterable[List[int]]:
+	for i in range(len(serie) - 1):
+		yield serie[i:i+2]
+
+def reduce(serie: List[int]) -> Iterable[int]:
+	for p1, p2 in pairwise(serie):
+		yield p2 - p1
+
+def predict_next(serie: List[int]) -> int:
+	next_number: int = serie[-1]
+	while any(n != 0 for n in serie):
+		serie = list(reduce(serie))
+		next_number += serie[-1]
+
+	return next_number
+
+def p1(args):
+	series = read_file(args.file)
+	print(sum(predict_next(serie) for serie in series))
+
+def p2(args):
+	_ = read_file(args.file)
+
+if __name__ == '__main__':
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('file', type=str, default='input.txt')
+	parser.add_argument('-v', '--verbose', type=str, choices={'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'}, default='WARNING')
+	args = parser.parse_args()
+
+	logging.basicConfig(level=args.verbose)
+
+	p1(args)
+	p2(args)
