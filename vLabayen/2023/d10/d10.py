a754b2ade8c8435495e070b2ce8c12c28e43a870
vlabayen <victor.labayen@naudit.es>
2023-12-12 01:32:19 +0100

    hate myself

--- vLabayen/2023/d10/d10.py
+++ vLabayen/2023/d10/d10.py
@@ -109,41 +109,88 @@ def get_loop(start: Pipe, pipes: Dict[Coordinate, Pipe]) -> List[Pipe]:
 
 	raise
 
-def resolve_start(x: int, y: int, path: Set[Coordinate]) -> Pipe:
-	for shape in {'L', 'J', '7', 'F'}:
+def resolve_start(x: int, y: int, prev: Coordinate, next: Coordinate) -> Pipe:
+	neighbours = {prev, next}
+	for shape in {'L', 'J', '7', 'F', '|', '-'}:
 		connected_neighbours: Iterable[Coordinate] = ((x + dx, y + dy) for dx, dy in pipes_shapes[shape])
-		if all(neighbour in path for neighbour in connected_neighbours):
+		if all(neighbour in neighbours for neighbour in connected_neighbours):
 			return Pipe(shape, x, y)
 	
 	raise
 
 
-class Rotation(Enum):
-	Clockwise        = auto()
-	CounterClockwise = auto()
+# class Rotation(Enum):
+# 	Clockwise        = auto()
+# 	CounterClockwise = auto()
 
-_corner_rotation = {
-	'L': lambda prev, next: Rotation.Clockwise if prev.y > next.y else Rotation.CounterClockwise,
-	'F': lambda prev, next: Rotation.Clockwise if prev.y > next.y else Rotation.CounterClockwise,
-	'J': lambda prev, next: Rotation.Clockwise if prev.y < next.y else Rotation.CounterClockwise,
-	'7': lambda prev, next: Rotation.Clockwise if prev.y < next.y else Rotation.CounterClockwise,
-}
-def rotation_direction(prev: Pipe, corner: Pipe, next: Pipe) -> Rotation:
-	return _corner_rotation[corner.shape](prev, next)
+# _corner_rotation = {
+# 	'L': lambda prev, next: Rotation.Clockwise if prev.y > next.y else Rotation.CounterClockwise,
+# 	'F': lambda prev, next: Rotation.Clockwise if prev.y > next.y else Rotation.CounterClockwise,
+# 	'J': lambda prev, next: Rotation.Clockwise if prev.y < next.y else Rotation.CounterClockwise,
+# 	'7': lambda prev, next: Rotation.Clockwise if prev.y < next.y else Rotation.CounterClockwise,
+# }
+# def rotation_direction(prev: Pipe, corner: Pipe, next: Pipe) -> Rotation:
+# 	return _corner_rotation[corner.shape](prev, next)
 
+def is_corner(shape: str) -> bool:
+	return shape in {'L', 'J', '7', 'F'}
+
+def same_direction(prev_shape: str, current_shape: str) -> bool:
+	if {prev_shape, current_shape} == {'L', '7'}: return True
+	if {prev_shape, current_shape} == {'J', 'F'}: return True
+	return False
+
+def print_map(loop: Dict[Coordinate, Pipe], inside_tiles: Set[Coordinate]) -> None:
+	min_x, max_x = min(pipe.x for pipe in loop.values()), max(pipe.x for pipe in loop.values())
+	min_y, max_y = min(pipe.y for pipe in loop.values()), max(pipe.y for pipe in loop.values())
+
+	for y in range(min_y, max_y + 1):
+		get_c = lambda x, y: loop[(x, y)].shape if (x, y) in loop else 'I' if (x, y) in inside_tiles else 'O'
+		print(''.join(get_c(x, y) for x in range(min_x, max_x + 1)))
 
 def p2(args):
 	pipes = read_file(args.file)
 	start = [pipe for pipe in pipes.values() if pipe.shape == 'S'][0]
 
 	loop = get_loop(start, pipes)
-	loop[loop.index(start)] = resolve_start(start.x, start.y, set((pipe.x, pipe.y) for pipe in loop))
-	for p in loop: print(p)
+	loop[-1] = resolve_start(start.x, start.y, (loop[0].x, loop[0].y), (loop[-2].x, loop[-2].y))
 
-	corners = ((loop[(i - 1) % len(loop)], p, loop[(i + 1) % len(loop)]) for i, p in enumerate(loop))
-	rotations = (rotation_direction(prev, corner, next) for prev, corner, next in corners if corner.shape in {'L', 'F', 'J', '7'})
-	c = Counter(rotations)
-	print(c)
+	min_x, max_x = min(pipe.x for pipe in loop), max(pipe.x for pipe in loop)
+	min_y, max_y = min(pipe.y for pipe in loop), max(pipe.y for pipe in loop)
+
+	loop_pipes = {(pipe.x, pipe.y): pipe for pipe in loop}
+	inside_tiles = set()
+	for y in range(min_y, max_y + 1):
+		currently_inside = False
+		prev_corner = None
+		for x in range(min_x, max_x + 1):
+			if (x, y) not in loop_pipes:
+				if currently_inside: inside_tiles.add((x, y))
+				continue
+			
+			pipe = loop_pipes[(x, y)]
+			if not is_corner(pipe.shape):
+				if prev_corner is None:
+					currently_inside = not currently_inside
+				continue
+
+			if prev_corner is None:
+				prev_corner = pipe.shape
+				continue
+	
+			if same_direction(prev_corner, pipe.shape):
+				currently_inside = not currently_inside
+			
+			prev_corner = None
+
+	print(len(inside_tiles))
+
+	# for p in loop: print(p)
+
+	# corners = ((loop[(i - 1) % len(loop)], p, loop[(i + 1) % len(loop)]) for i, p in enumerate(loop))
+	# rotations = (rotation_direction(prev, corner, next) for prev, corner, next in corners if corner.shape in {'L', 'F', 'J', '7'})
+	# c = Counter(rotations)
+	# print(c)
 
 if __name__ == '__main__':
 	import argparse
